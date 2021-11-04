998c9b70e62bf502b913a9bb108c94cf909a6766
kotovi4 <kotovi4.m@gmail.com>
2021-11-04 17:46:18 +0300

    добавляет валидацию формы обратной связи

--- source/js/script.js
+++ source/js/script.js
@@ -3,9 +3,23 @@
 //меню tablet и mobile
 const pageHeader = document.querySelector('.page-header');
 const headerToggle = document.querySelector('.page-header__toggle');
+const callbackForm = document.querySelector('.callback__form');
+const callbackEmailInput = document.querySelector('#callback-email');
+const callbackTelInput = document.querySelector('#callback-tel');
+const callbackButton = callbackForm.querySelector('.callback__button')
+const countryLinks = document.querySelectorAll('.country__link');
+const countryCards = document.querySelectorAll('.country__tabs-item');
+const countryIcons = document.querySelectorAll('.location__link')
+const buttonBuyNow = document.querySelector('.button--buy-now');
+const modal = document.querySelector('.modal');
+const modalSuccess = modal.querySelector('.modal__success');
+const modalInputTel = modal.querySelector('.modal__input--tel');
+const modalClose = modal.querySelector('.modal__close');
+
 
 pageHeader.classList.remove('page-header--nojs');
 
+//открытие и закрытие мобильного меню
 headerToggle.addEventListener('click', function () {
   if (pageHeader.classList.contains('page-header--closed')) {
     pageHeader.classList.remove('page-header--closed');
@@ -17,10 +31,6 @@ headerToggle.addEventListener('click', function () {
 });
 
 //переключение карточек стран
-const countryLinks = document.querySelectorAll('.country__link');
-const countryCards = document.querySelectorAll('.country__tabs-item');
-const countryIcons = document.querySelectorAll('.location__link')
-
 countryCards.forEach((card) => {
   card.classList.add('country__tabs-item--hidden');
 });
@@ -52,20 +62,41 @@ countryIcons.forEach((element, index) => {
   });
 });
 
-//модальное окно
-const modal = document.querySelector('.modal');
-const modalSuccess = modal.querySelector('.modal__success');
-const modalInputTel = modal.querySelector('.modal__input--tel');
-const modalClose = modal.querySelector('.modal__close');
-const buttonBuyNow = document.querySelector('.button--buy-now');
+//открытие модального окна
+const popupShow = function (evt) {
+  if (evt.target.classList.contains("button--buy-now")) {
+    modal.classList.add("modal--show");
+  }
+};
 
-buttonBuyNow.onclick = () => {
-  modal.classList.remove('modal--show');
-  modalSuccess.classList.remove('modal__success--show');
-}
+//закрытие модального окна
+const popupClose = function () {
+  modal.classList.remove("modal--show");
+};
 
-//закрыть модальное окно
-modalClose.onclick = () => {
-  modal.classList.remove('modal--show');
-  modalSuccess.classList.remove('modal__success--show');
+modalClose.addEventListener("click", popupClose);
+document.addEventListener("click", popupShow);
+
+//Валидация полей формы обратной связи
+if (callbackEmailInput && callbackTelInput) {
+  callbackEmailInput.addEventListener('input', function () {
+    if (callbackEmailInput.validity.patternMismatch) {
+      callbackEmailInput.setCustomValidity('Вводите только латинские буквы');
+    } else {
+      callbackEmailInput.setCustomValidity('');
+    }
+  });
+
+  callbackTelInput.addEventListener('input', function () {
+    if (callbackTelInput.validity.patternMismatch) {
+      callbackTelInput.setCustomValidity('Введите телефон в формате +7 ХХХ ХХХ ХХ ХХ');
+    } else {
+      callbackTelInput.setCustomValidity('');
+    }
+  });
+
+  callbackButton.addEventListener('click', function () {
+    localStorage.setItem(callbackEmailInput.name, callbackEmailInput.value);
+    localStorage.setItem(callbackTelInput.name, callbackTelInput.value);
+  });
 }
